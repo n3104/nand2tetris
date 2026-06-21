b293f692cd24c37c4d366539873af4b882a33cf2
n3104 <nt3104@gmail.com>
2026-06-21 16:25:08 +0900

    Mult.asm を実装した

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,40 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+// RAM[2]にRAM[0]*RAM[1]を計算する。具体的にはRAM[0]をRAM[1]回足す
+    // i = 1
+    @i
+    M=1
+    // sum = 0
+    @sum
+    M=0
+(LOOP)
+    // if (i > R1) goto STOP
+    @i
+    D=M
+    @R1
+    D=D-M
+    @STOP
+    D;JGT
+    // sum = sum + R0
+    @sum
+    D=M
+    @R0
+    D=D+M
+    @sum
+    M=D
+    // i = i + 1
+    @i
+    M=M+1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(STOP)
+    // R2 = sum
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
