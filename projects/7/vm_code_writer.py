f21e0b2d104014a33143ec09b9f4c1fc44f7beb5
n3104 <nt3104@gmail.com>
2026-09-13 22:59:51 +0900

    SimpleAddを前提としたVM変換器を作ってみた

---
+++ projects/7/vm_code_writer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+class CodeWriter:
+
+    def __init__(self, file_path: Path, encoding: str = "utf-8"):
+        self._f_out = file_path.open("w", encoding="utf-8")
+        f_out = self._f_out
+        f_out.write(f"// set stack base address\n")
+        f_out.write(f"@256\n")
+        f_out.write(f"D=A\n")
+        f_out.write(f"@SP\n")
+        f_out.write(f"M=D\n")
+
+    def write_arithmetic(self, command: str) -> None:
+        f_out = self._f_out
+        if command == "add":
+            f_out.write(f"// add\n")
+            f_out.write(f"@SP\n")
+            f_out.write(f"M=M-1\n")
+            f_out.write(f"A=M\n")
+            f_out.write(f"D=M\n")
+            f_out.write(f"@SP\n")
+            f_out.write(f"M=M-1\n")
+            f_out.write(f"A=M\n")
+            f_out.write(f"M=D+M\n")
+            f_out.write(f"@SP\n")
+            f_out.write(f"M=M+1\n")
+        else:
+            raise RuntimeError(f"サポート外のコマンドです: {command}")
+
+    def write_push_pop(self, command: str, segment:str, index: int) -> None:
+        f_out = self._f_out
+        if command == "C_PUSH":
+            f_out.write(f"// push {segment} {index}\n")
+            if segment == "constant":
+                f_out.write(f"@{index}\n")
+                f_out.write(f"D=A\n")
+                f_out.write(f"@SP\n")
+                f_out.write(f"A=M\n")
+                f_out.write(f"M=D\n")
+                f_out.write(f"@SP\n")
+                f_out.write(f"M=M+1\n")
+                return
+        elif command == "C_POP":
+            return
+        else:
+            raise RuntimeError(f"サポート外のコマンドです: {command}")
+
+    def close(self) -> None:
+        return self._f_out.close()
---
+++ projects/7/vm_parser.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+class Parser:
+
+    def __init__(self, file_path: str, encoding: str = "utf-8"):
+        file_path = Path(file_path)
+        if not file_path.exists():
+            raise FileNotFoundError(f"ファイルが見つかりません: {self.file_path}")
+
+        self._lines = file_path.read_text(encoding).splitlines()
+        self._line_number = 0
+        self._current_line = None
+
+    def has_more_lines̶(self) -> bool:
+        return self._line_number < len(self._lines)
+
+    def advance̶(self) -> None:
+        """次の行に進める（空白とコメントはスキップする）"""
+        while True:
+            # EOF (ファイル末尾) に達した場合
+            if self.has_more_lines̶() == False:
+                return
+
+            line = self._lines[self._line_number]
+            self._line_number += 1
+            print(line)
+
+            # コメント部分を除去
+            code_part = line.split("//")[0]
+            # 空白と改行コードを取り除いて空行かどうか判定
+            cleaned_line = code_part.strip()
+            
+            # 空行とコメントならループを継続し、次の行へ進む
+            if not cleaned_line:
+                continue
+
+            # 命令行が見つかったら現在行に設定する
+            self._current_line = cleaned_line
+            return
+
+    def command_type(self) -> str:
+        """現在のコマンドのタイプを返す"""
+        line = self._current_line
+        if line.startswith("push"):
+            return "C_PUSH"
+        elif line.startswith("pop"):
+            return "C_POP"
+        else:
+            return "C_ARITHMETIC"
+
+    def arg1(self) -> str:
+        """現在のコマンドの第1引数を返す"""
+        command_type = self.command_type()
+        line = self._current_line
+        if command_type == "C_PUSH" or command_type == "C_POP":
+            return line.split()[1]
+        elif command_type == "C_ARITHMETIC":
+            return line
+        else:
+            raise RuntimeError(f"サポート外のコマンドです: {command_type}")
+
+    def arg2(self) -> str:
+        """現在のコマンドの第2引数を返す"""
+        command_type = self.command_type()
+        line = self._current_line
+        if command_type == "C_PUSH" or command_type == "C_POP":
+            return line.split()[2]
+        else:
+            raise RuntimeError(f"サポート外のコマンドです: {command_type}")
